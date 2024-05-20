c3cf0f8643b0f5b1f24a7466a74339c0aad46a5b
carlos.gomez <drcgomez@hotmail.com>
2024-05-20 18:52:25 -0500

    pruebas para servicios

---
+++ src/app/services/value.service.spec.ts
@@ -0,0 +1,56 @@
+import { firstValueFrom } from 'rxjs';
+import { ValueService } from './value.service';
+
+fdescribe('ValueService', () => {
+  let service: ValueService;
+
+  beforeEach(() => {
+    service = new ValueService();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('test for getValue', () => {
+    it('should return the real value', () => {
+      expect(service.getValue()).toBe('real value');
+    });
+  });
+
+  describe('test for setValue', () => {
+    it('should change the value', () => {
+      service.setValue('new value');
+      expect(service.getValue()).toBe('new value');
+      expect(service.getValue()).not.toBe('real value');
+    });
+  });
+
+  describe('test for getPromiseValue', () => {
+    it('should return "promise value" from promise with then', (doneFn) => {
+      service.getPromiseValue().then((value) => {
+        expect(value).toBe('promise value');
+        doneFn();
+      });
+    });
+
+    it('should return "promise value" from promise using async', async () => {
+      const value = await service.getPromiseValue();
+      expect(value).toBe('promise value');
+    });
+  });
+
+  describe('test for getObservableValue', () => {
+    it('should return "observable value" from observable', (doneFn) => {
+      service.getObservableValue().subscribe((value) => {
+        expect(value).toBe('observable value');
+        doneFn();
+      });
+    });
+
+    it('should return "observable value" from observable using async', async () => {
+      const value = await firstValueFrom(service.getObservableValue());
+      expect(value).toBe('observable value');
+    });
+  });
+});
