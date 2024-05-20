0e6813fb6c3556a8c1b22d1ef57738fb2e3f0082
carlos.gomez <drcgomez@hotmail.com>
2024-05-20 18:24:04 -0500

    mocha report

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -7,42 +7,52 @@ describe('Test for calculator', () => {
     calculator = new Calculator();
   });
 
-  it('#multiply should return 6', () => {
-    const result = calculator.multiply(2, 3);
-    expect(result).toEqual(6);
+  describe('Test for multiply', () => {
+    it('#multiply should return 6', () => {
+      const result = calculator.multiply(2, 3);
+      expect(result).toEqual(6);
+    });
+
+    it('#multiply should return 0', () => {
+      const result = calculator.multiply(2, 0);
+      expect(result).toEqual(0);
+    });
   });
 
-  it('#multiply should return 0', () => {
-    const result = calculator.multiply(2, 0);
-    expect(result).toEqual(0);
+  describe('Test for divide', () => {
+    it('#divide should return 2', () => {
+      const result = calculator.divide(6, 3);
+      expect(result).toEqual(2);
+    });
+
+    it('#divide should throw an error', () => {
+      expect(() => calculator.divide(6, 0)).toThrowError(
+        'Cannot divide by zero'
+      );
+    });
   });
 
-  it('#divide should return 2', () => {
-    const result = calculator.divide(6, 3);
-    expect(result).toEqual(2);
+  describe('Test for add', () => {
+    it('#add should return 3', () => {
+      const result = calculator.add(1, 2);
+      expect(result).toEqual(3);
+    });
+
+    it('#add should return 0', () => {
+      const result = calculator.add(-1, 1);
+      expect(result).toEqual(0);
+    });
   });
 
-  it('#divide should throw an error', () => {
-    expect(() => calculator.divide(6, 0)).toThrowError('Cannot divide by zero');
-  });
+  describe('Test for subtract', () => {
+    it('#subtract should return -1', () => {
+      const result = calculator.subtract(1, 2);
+      expect(result).toEqual(-1);
+    });
 
-  it('#add should return 3', () => {
-    const result = calculator.add(1, 2);
-    expect(result).toEqual(3);
-  });
-
-  it('#add should return 0', () => {
-    const result = calculator.add(-1, 1);
-    expect(result).toEqual(0);
-  });
-
-  it('#subtract should return -1', () => {
-    const result = calculator.subtract(1, 2);
-    expect(result).toEqual(-1);
-  });
-
-  it('#subtract should return 0', () => {
-    const result = calculator.subtract(1, 1);
-    expect(result).toEqual(0);
+    it('#subtract should return 0', () => {
+      const result = calculator.subtract(1, 1);
+      expect(result).toEqual(0);
+    });
   });
 });
