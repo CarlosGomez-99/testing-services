775f86584bd29d722a07030f550cc1840cb8e761
carlos.gomez <drcgomez@hotmail.com>
2024-05-20 18:14:06 -0500

    reporte de coverage

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -9,6 +9,40 @@ describe('Test for calculator', () => {
 
   it('#multiply should return 6', () => {
     const result = calculator.multiply(2, 3);
-    expect(result).toBe(6);
+    expect(result).toEqual(6);
+  });
+
+  it('#multiply should return 0', () => {
+    const result = calculator.multiply(2, 0);
+    expect(result).toEqual(0);
+  });
+
+  it('#divide should return 2', () => {
+    const result = calculator.divide(6, 3);
+    expect(result).toEqual(2);
+  });
+
+  it('#divide should throw an error', () => {
+    expect(() => calculator.divide(6, 0)).toThrowError('Cannot divide by zero');
+  });
+
+  it('#add should return 3', () => {
+    const result = calculator.add(1, 2);
+    expect(result).toEqual(3);
+  });
+
+  it('#add should return 0', () => {
+    const result = calculator.add(-1, 1);
+    expect(result).toEqual(0);
+  });
+
+  it('#subtract should return -1', () => {
+    const result = calculator.subtract(1, 2);
+    expect(result).toEqual(-1);
+  });
+
+  it('#subtract should return 0', () => {
+    const result = calculator.subtract(1, 1);
+    expect(result).toEqual(0);
   });
 });
