397a7ab91b052945a514e725072914c72634363c
carlos.gomez <drcgomez@hotmail.com>
2024-05-21 20:13:00 -0500

    TestBed

--- src/app/services/value.service.spec.ts
+++ src/app/services/value.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { firstValueFrom } from 'rxjs';
 import { ValueService } from './value.service';
 
@@ -5,7 +6,10 @@ describe('ValueService', () => {
   let service: ValueService;
 
   beforeEach(() => {
-    service = new ValueService();
+    TestBed.configureTestingModule({
+      providers: [ValueService]
+    });
+    service = TestBed.inject(ValueService);
   });
 
   it('should be created', () => {
