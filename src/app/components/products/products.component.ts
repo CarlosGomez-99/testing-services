db601eecda1932c709ea0540436e5bf74e8ad78e
carlos.gomez <drcgomez@hotmail.com>
2024-05-22 21:30:13 -0500

    product service http

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './../../models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productsService.getAllSimple().subscribe((products) => {
+      this.products = products.map((product) => {
+        let images = product.images.map((image) => {
+          return image.replace('["', '').replace('"]', '');
+        });
+        return { ...product, images };
+      });
+    });
+  }
 }
